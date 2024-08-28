0cd997de88d9315d8f3a03957157a3281175975f
hyewon <hyewon.parkhw@gmail.com>
2024-08-28 16:12:07 +0900

    add: terraform code

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "argocd_admin_password" {
+  default = "New1234!"
+  type = string
+}
+
+variable "destination_namespace" {
+  default = "hello"
+  type = string
+}
+
+variable "azure_resource_group_name" {
+  default = 
+  type = string
+}
+
+variable "azure_cluster_name" {
+  default = 
+  type = string
+}
+
+variable "azure_registry_name" {
+  default = 
+  type = string
+}
+
+variable "github_repository" {
+  default = 
+  type = string
+}
+
