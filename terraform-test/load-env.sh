c85da63d53bb7951b3bd349a59bcea470517abc4
김용재(ICT CoE팀) <yong_jae.kim@kt.com>
2024-09-09 15:53:08 +0900

    bash 공통버전으로 수정

--- terraform-test/load-env.sh
+++ terraform-test/load-env.sh
@@ -3,10 +3,10 @@
 # .env 파일을 로드하여 환경 변수 설정
 export $(grep -v '^#' config.env | xargs)
 
-
+# 임시 파일 생성
+temp_file=$(mktemp)
 
 if [ -f config.env ]; then
-    declare -A variables  # 변수 저장을 위한 associative 배열
     error_found=false  # 오류 여부를 추적하는 플래그
 
     while IFS= read -r line; do
@@ -22,43 +22,48 @@ if [ -f config.env ]; then
                 echo "오류: 변수 $var_name 값이 비어 있습니다."
                 error_found=true
             fi
+
+            # 변수와 값 저장 (디버깅용 출력 및 임시 파일에 기록)
+            echo "var_name: $var_name, var_value: $var_value"
+            echo "export $var_name=\"$var_value\"" >> "$temp_file"
         fi
     done < config.env
 
     # 빈값 오류가 있으면 스크립트 중단
     if $error_found; then
         echo "오류가 발생하여 스크립트를 중단합니다."
-        # 스크립트 끝에서 대기 상태 유지
-        read -p "Press any key to continue..."
+        # 스크립트 끝에서 대기 상태 유지 (터미널 종료 방지)
+        read -p "Press any key to continue..."        
+        rm -f "$temp_file"  # 임시 파일 삭제
         exit 1
     fi
 
     # 모든 변수가 올바르게 설정되었는지 확인
     echo "모든 변수가 다음과 같이 설정되었습니다:"
-    for var_name in "${!variables[@]}"; do
-        echo "$var_name=${variables[$var_name]}"
-    done
+    grep -v '^#' config.env
 
     # 사용자 확인 (yes/no)
     read -p "이 값들이 맞습니까? (yes/no): " answer
     if [[ "$answer" != "yes" ]]; then
         echo "사용자에 의해 스크립트가 중단되었습니다."
-        # 스크립트 끝에서 대기 상태 유지
+        # 스크립트 끝에서 대기 상태 유지 (터미널 종료 방지)
         read -p "Press any key to continue..."        
+        rm -f "$temp_file"  # 임시 파일 삭제
         exit 1
     fi
 
-    # 확인 후 변수들을 export
-    for var_name in "${!variables[@]}"; do
-        export "$var_name=${variables[$var_name]}"
-    done
+    # 확인 후 임시 파일에서 변수를 export
+    source "$temp_file"
+    rm -f "$temp_file"  # 임시 파일 삭제
 
     echo "환경 변수가 성공적으로 설정되었습니다."
 else
     echo "config.env 파일이 존재하지 않습니다."
+    # 스크립트 끝에서 대기 상태 유지 (터미널 종료 방지)
+    read -p "Press any key to continue..."
+    rm -f "$temp_file"  # 임시 파일 삭제
     exit 1
 fi
 
 terraform init
-
 terraform apply
