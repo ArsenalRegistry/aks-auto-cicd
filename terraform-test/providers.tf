205f9076597794bcd9415fb94765fdeec522bfd0
ArsenalRegistry <132525420+ArsenalRegistry@users.noreply.github.com>
2024-09-26 14:59:13 +0900

    Update providers.tf

--- terraform-test/providers.tf
+++ terraform-test/providers.tf
@@ -14,10 +14,12 @@ provider "github" {
 provider "null" {}
 
 provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.aks.kube_config.0.host
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+  config_path = "~/.kube/config"
+  config_context="aks-az01-dev-vvd-01"
+  # host                   = data.azurerm_kubernetes_cluster.aks.kube_config.0.host
+  # client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  # client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  # cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
 }
 
 provider "argocd" {
